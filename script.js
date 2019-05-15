2c4825ef67bbc3d2dbe9a1f7ec0a300d8479fd19
huqingping <516604597@qq.com>
2019-05-15 23:03:55 +0800

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+// Hey! Add code to this file by following the steps in the README of this project 🌟
+// If you get stuck at any time, you can see a completed file in ./complete_files/script.js
+
+
+
+
+
+
+
---
+++ service-worker.js
@@ -0,0 +1,2 @@
+// Hey! Add code to this file by following the steps in the README of this project 🌟
+// If you get stuck at any time, you can see a completed file in ./complete_files/service-worker.js
